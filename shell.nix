a308533463d22ee2535ed1d0fcf852f879854b0d
Michael Burij <michael.burij@gmail.com>
2024-09-18 14:14:28 +0200

    Added autoupdate of lib.lua via nix-shell

--- shell.nix
+++ shell.nix
@@ -5,15 +5,19 @@ in
 
 pkgs.mkShell {
 	packages = with pkgs; [
+		wget
 		(lua5_4.withPackages(ps: with ps; [
 			luarocks
 		]))
 	];
 
 	shellHook = ''
+		wget -O lib.lua https://raw.githubusercontent.com/burij/milua/main/lib.lua
 		alias run='lua main.lua'
 		alias test='lua draft.lua'
 		luarocks install inspect --tree ./pkgs
+    		luarocks install lua-cjson --tree ./pkgs
+
 		export LUA_PATH="./pkgs/share/lua/5.4/?.lua;./share/lua/5.4/?/init.lua;$LUA_PATH"
 		export LUA_CPATH="./pkgs/lib/lua/5.4/?.so;$LUA_CPATH"
 	'';
