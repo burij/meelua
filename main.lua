5aec2478935ab728fc00f851ecab8a0b869deb1f
Michael Burij <michael.burij@gmail.com>
2024-10-10 14:24:54 +0200

    Compatibility with Löve2D
    Updates, which are making it easier to use meelua with Löve2D: config.lua was renamed to conf.lua, optional boilerplate functions were added to make it faster to switch to Löve2D execution.

--- main.lua
+++ main.lua
@@ -1,14 +1,23 @@
-local config, lib = require( "config" ), require( "lib" )
+local conf, lib = require( "conf" ), require( "lib" )
 --------------------------------------------------------------------------------
--- config
+-- conf
 
 --------------------------------------------------------------------------------
 -- lib
+
+-- function lib.do_gui(x, y, z)
+--     lib.types(x, "function")
+--     lib.types(y, "function")
+--     lib.types(z, "function")
+--     function love.load() x end
+--     function love.update(dt) y end
+--     function love.draw() z end
+-- end
 
 --------------------------------------------------------------------------------
 
 local function application()
     lib.do_print_table(lib)
-
+    -- lib.do_gui()
 end application()
 
--- shell.nix
+++ shell.nix
@@ -6,6 +6,7 @@ in
 pkgs.mkShell {
 	packages = with pkgs; [
 		wget
+		-- love
 		(lua5_4.withPackages(ps: with ps; [
 			luarocks
 		]))
@@ -14,9 +15,10 @@ pkgs.mkShell {
 	shellHook = ''
 		wget -O lib.lua https://raw.githubusercontent.com/burij/meelua/main/lib.lua
 		alias run='lua main.lua'
+		-- alias run='love .'
 		alias test='lua draft.lua'
 		luarocks install inspect --tree ./pkgs
-    	luarocks install dkjson --tree ./pkgs
+    		luarocks install dkjson --tree ./pkgs
 		export LUA_PATH="./pkgs/share/lua/5.4/?.lua;./share/lua/5.4/?/init.lua;$LUA_PATH"
 		export LUA_CPATH="./pkgs/lib/lua/5.4/?.so;$LUA_CPATH"
 	'';
