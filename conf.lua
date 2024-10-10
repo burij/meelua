23a0cb0c5b1eea1076bdbc5b375c368aa83da2bc
Michael Burij <michael.burij@gmail.com>
2024-10-10 17:20:43 +0200

    Wrong usage of t.version fixed

--- conf.lua
+++ conf.lua
@@ -4,10 +4,9 @@ conf = {}
 -- function love.conf(t)
 -- 	t.title				= "Demo App"
 -- 	t.author			= "Demo"
--- 	t.version			= 0.1
 -- 	t.screen.width		= 720
 -- 	t.screen.height		= 480
---     t.window.resizable  = true
+--  t.window.resizable  = true
 -- end
 
 --------------------------------------------------------------------------------
